7393136c1b9039624b70e4855ec92c5d98f7fdc9
Justin Lefrançois <38826625+Jusolijus17@users.noreply.github.com>
2025-01-29 18:13:29 -0500

    Tp mercredi 29

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -5,8 +5,47 @@
 
 void ASDTAIController::Tick(float deltaTime)
 {
-
+	MoveToTarget(FVector2D(0,0), m_maxSpeed, deltaTime);
 }
+
+bool ASDTAIController::MoveToTarget(FVector2D target, float speed, float deltaTime){
+	APawn* pawn = GetPawn();
+	FVector const pawnPosition(pawn->GetActorLocation());
+	FVector2D const toTarget = target - FVector2D(pawnPosition);
+	FVector2D const displacement = FMath::Min(toTarget.Size(), speed * deltaTime) * toTarget.GetSafeNormal();
+	pawn->SetActorLocation(pawnPosition + FVector(displacement, 0.f), true);
+	pawn->SetActorRotation(FVector(displacement, 0).ToOrientationQuat());
+	return toTarget.Size() < speed * deltaTime;
+}
+
+bool ASDTAIController::IsInsideCone(APawn* pawn, AWallActor* targetActor) const
+{
+    if (FVector::Dist2D(pawn->GetActorLocation(), targetActor->GetActorLocation()) > 500.f)
+    {
+        return false;
+    }
+
+    auto pawnForwardVector = pawn->GetActorForwardVector();
+    auto dir = targetActor->GetActorLocation() - pawn->GetActorLocation();
+
+    auto value = FVector::DotProduct(dir.GetSafeNormal(), pawnForwardVector.GetSafeNormal());
+    auto angle = FMath::Acos(value);
+
+    auto isVisible = FMath::Abs(angle) <= m_visionAngle;
+
+    if (isVisible)
+    {
+        DrawDebugSphere(GetWorld(), targetActor->GetActorLocation(), 100.f, 32, FColor::Magenta);
+    }
+
+    GEngine->AddOnScreenDebugMessage(-1, 0.16f, FColor::Red, FString::Printf(TEXT("Actor : %s is %s"), *targetActor->GetName(), isVisible ? TEXT("visible") : TEXT("not visible")));
+
+    return isVisible;
+}
+
+
+
+
 
 
 
--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -14,6 +14,20 @@ UCLASS(ClassGroup = AI, config = Game)
 class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 {
     GENERATED_BODY()
+
+private:
+    virtual bool MoveToTarget(FVector2D target, float speed, float deltaTime);
+    virtual void GetVisibilityInformationForTarget(AWallActor* targetActor, FVector selfPosition, PhysicsHelpers& physicHelper, bool& canSee, bool& canShoot);
+    virtual void DrawCharacterAxes(UWorld* world, APawn* pawn) const;
+    virtual void DrawVisionCone(UWorld* world, APawn* pawn, FVector const& dir, float angle) const;
+    virtual TArray<FOverlapResult> CollectTargetActorsInFrontOfCharacter(APawn const* pawn, PhysicsHelpers& physicHelper) const;
+    virtual bool IsInsideCone(APawn* pawn, AWallActor* targetActor) const;
 public:
     virtual void Tick(float deltaTime) override;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Movement")
+    float m_maxSpeed = 500.0f;
+
+    float const m_maxVisionAngle = PI / 3.0f;
 };
