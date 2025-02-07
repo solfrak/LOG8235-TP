061bc5a9219075de92c8da97e8364973ec1e6f22
Samuel <laporte.samuel2000@gmail.com>
2025-02-07 11:27:08 -0500

    it does something

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -8,198 +8,85 @@
 #include "SDTCollectible.h"
 
 
-FVector ASDTAIController::GetMovementDirection() const
+void ASDTAIController::DoMovement(float deltaTime, APawn* pawn)
 {
-    APawn* ControlledPawn = GetPawn();
-    if (ControlledPawn)
-        return ControlledPawn->GetActorForwardVector();
-    else
-        return FVector::ForwardVector;
-}
-
-void ASDTAIController::UpdateVelocity(float deltaTime)
-{
-    //need to change le maxwalkspeed du character ici.
-
-    current_speed += m_accel * deltaTime;
-
-    if (current_speed > m_max_speed) {
-        current_speed = m_max_speed;
-    }
-}
-
-
-void ASDTAIController::ApplyMovement(FVector direction)
-{
-    FVector normalized = direction.GetSafeNormal();
-    APawn* ControlledPawn = GetPawn();
-    if (ControlledPawn)
+    // Apply a forward acceleration until max speed is reached
+    FVector forward_direction = pawn->GetActorForwardVector();
+    float current_speed = m_velocity.Size();
+    
+    if (current_speed < m_max_speed) 
     {
-        ControlledPawn->AddMovementInput(normalized, current_speed);
+        m_velocity += forward_direction * m_acceleration * deltaTime;
     }
-}
 
-void ASDTAIController::ApplyRotation(FVector direction)
-{
-    APawn* ControlledPawn = GetPawn();
-    if (ControlledPawn)
-    {
-        //convert speed vect to rotation
-        FRotator currentRotation = GetMovementDirection().Rotation();
-        FRotator TargetRotation = direction.Rotation();
-        FRotator diffRotation = TargetRotation - currentRotation;
-        FHitResult SweepHit;
+    // Clamp velocity to max speed
+    m_velocity = m_velocity.GetClampedToMaxSize(m_max_speed);
+    m_velocity.Z = 0.0f;
 
-        ControlledPawn->AddActorWorldRotation(diffRotation, true, &SweepHit, ETeleportType::None);
+    // Move the pawn
+    pawn->AddMovementInput(m_velocity.GetSafeNormal(), 1.0f);
 
-    }
-}
-
-void ASDTAIController::AvoidObstacle(float deltaTime) {
-	//a fixer ici because ai is too stupid and goes in circle sometimes
-    //will need to change direction in a smarter way
-	m_current_transition_duration += deltaTime;
-	float ratio = deltaTime / m_transition_duration;
-	
-	APawn* pawn = GetPawn();
-	FVector const pawnPosition(pawn->GetActorLocation());
-	FVector const forward(pawn->GetActorForwardVector());
-	FVector const side(pawn->GetActorRightVector()); //jutilise side ici (basically un 90 degre), mais no idea sil faut un certain angle selon l'enonce du tp
-
-	FVector const newDir = FMath::Lerp(forward, side, ratio);
-    ApplyMovement(newDir);
-    ApplyRotation(newDir);
+    // Smooth rotation towards movement direction
+    const FRotator current_rot = pawn->GetActorRotation();
+    const FRotator target_rot = m_velocity.Rotation();
+    const FRotator result = FMath::Lerp(current_rot, target_rot, 0.05f);
+    pawn->SetActorRotation(result);
 }
 
 void ASDTAIController::Tick(float deltaTime)
 {
     Super::Tick(deltaTime);
+    APawn *pawn = GetPawn();
 
-    UpdateVelocity(deltaTime);
-    MoveToTarget(FVector2D(0, 0), current_speed, deltaTime);
-    //m_wall_detected = DetectWall();
-    if (false) {
-        //AvoidObstacle(deltaTime);
-    }
-    else {
-        FVector vector;
-        //maybe re align direction with closest horizontal axis
-        m_current_transition_duration = 0.0f;
-        //ApplyMovement(GetMovementDirection());
-        //ApplyRotation(GetMovementDirection());
-        //DetectPickup(vector);
-    }
-
-
-    //doit dodge les obstacles dans tous ces states
-    /**
-    if player detected
-        if player powered up
-            Fuite
-        else
-            Poursuite
-    else if pickup detected
-        Pickup
-    else
-        Patrouille
-    
-    **/
-
-    /*
-    target position
-    agent va la bas en evitant les obstacles
-    */
-
-
-}
-
-//pas utiliser pour le moment, on veut ajouter detection obstacle
-bool ASDTAIController::MoveToTarget(FVector2D target, float speed, float deltaTime)
-{
-
-    //IN HERE WE WANT TO MOVE THE CHARACTER A LITTLE BIT EACH TIME
-    //CHECK FOR WALL
-    //IF WALL THEN WE WANT TO AVOID MANEUVER
-
-    APawn* pawn = GetPawn();
-    FVector const pawnPosition(pawn->GetActorLocation());
-    FVector2D const toTarget = target - FVector2D(pawnPosition);
-
-    FVector2D const displacement = FMath::Min(toTarget.Size(), speed * deltaTime) * toTarget.GetSafeNormal();
-    
-    ApplyMovement(FVector(displacement, 0));
-    ApplyRotation(FVector(displacement, 0));
-    FVector normal;
-    if (DetectObstacle(normal)) {
-        //vecteur de la direction quon voudrait aller, order matters pour cross product.
-        FVector maneuverDirection = FVector::CrossProduct(normal.GetSafeNormal(), pawn->GetActorUpVector()).GetSafeNormal();
-        //do something acceleration here
-    }
-    return toTarget.Size() < speed * deltaTime;
-}
-
-
-bool ASDTAIController::DetectObstacle(FVector& normal)
-{
-    PhysicsHelpers helper(GetWorld());
-
-    APawn* pawn = GetPawn();
-    FVector const pawnPosition(pawn->GetActorLocation());
-    TArray<FHitResult> hits;
-
-    //faudra revoir pour ca parce qu'on met le collision channel ici mais jai hard coder dans physicshelper l'autre channel
-    helper.CapsuleCast(pawnPosition, pawnPosition + pawn->GetActorForwardVector() * m_wall_detection_distance, 50.0f, hits, COLLISION_DEATH_OBJECT, true);
-    
-    float distance = FLT_MAX;
-    //debug purpose
-    for (const FHitResult& hit : hits)
+    //Detect Wall
+    auto hits = GetObstacleInPath();
+    for (auto hit : hits)
     {
-        //need to cast and still check if death traps or walls? cast should arealdy do that
-        float hitDistance = (hit.ImpactPoint - pawnPosition).Size();
-        if (hitDistance < distance) {
-            normal = hit.ImpactNormal;
-        }
+        m_velocity += hit.ImpactNormal * (1.0f / hit.Distance) * 50;
     }
-    return hits.Num() != 0;
 
+    //Check for collectable
+    MoveTowardCollectable();
+    
+
+    DoMovement(deltaTime, pawn);
 }
 
-
-
-bool ASDTAIController::DetectPickup(FVector& vector) 
+TArray<FHitResult> ASDTAIController::GetObstacleInPath()
 {
-    PhysicsHelpers helper(GetWorld());
+    APawn *pawn = GetPawn();
+    FVector current_pos = pawn->GetActorLocation();
+    TArray<FHitResult> results;
     
-    APawn* ControlledPawn = GetPawn();
-    if (!ControlledPawn)
-    {
-        return false;
-    }
+    //TODO: use physics helper to make a sweep
+    PhysicsHelpers Helpers(GetWorld());
+    Helpers.CapsuleCast(current_pos, current_pos + pawn->GetActorForwardVector() * 200, 30, results, ECC_Visibility, true);
 
-    FVector PawnLocation = ControlledPawn->GetActorLocation();
-
-    TArray<FOverlapResult> OverlapResults;
-    bool bOverlapped = helper.SphereOverlap(PawnLocation + ControlledPawn->GetActorForwardVector() * m_pickup_detection_range, m_pickup_detection_radius, OverlapResults, COLLISION_COLLECTIBLE, true);
-    
-    for (const FOverlapResult& Result : OverlapResults)
-    {
-        AActor* OverlappedActor = Result.GetActor();
-        if (!OverlappedActor)
-        {
-            continue;
-        }
-
-        ASDTCollectible* Pickup = Cast<ASDTCollectible>(OverlappedActor);
-        if (Pickup && !Pickup->IsOnCooldown())
-        {
-            bool bObstacle = SDTUtils::Raycast(GetWorld(), PawnLocation, Pickup->GetActorLocation());
-            
-            if (!bObstacle) {
-                vector = Pickup->GetActorLocation();
-                return true;
-            }
-        }
-    }
-
-    return false;
+    return results;
 }
+
+void ASDTAIController::MoveTowardCollectable()
+{
+    APawn *pawn = GetPawn();
+    PhysicsHelpers Helpers(GetWorld());
+
+    TArray<FOverlapResult> results;
+    Helpers.SphereOverlap(pawn->GetActorLocation() + pawn->GetActorForwardVector() * 200, 200, results, COLLISION_COLLECTIBLE, true);
+
+    if (results.Num() > 0) 
+    {
+        // Get first collectible position
+        FVector targetLocation = results[0].GetActor()->GetActorLocation();
+        FVector direction = (targetLocation - pawn->GetActorLocation()).GetSafeNormal();
+
+        // Lock movement to 2D
+        direction.Z = 0.0f;
+
+        // Apply force to steer toward the collectible
+        m_velocity += direction * m_acceleration;
+
+        // Clamp velocity to max speed
+        m_velocity = m_velocity.GetClampedToMaxSize(m_max_speed);
+    }
+}
+
--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -14,79 +14,15 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 {
     GENERATED_BODY()    //generates additional code for unreal
 public:
-
-    //Macro pour intéragir avec blueprint du éditeur
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-    FVector MovementDirection;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-    float Acceleration;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-    float MaxSpeed;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-    FVector Velocity;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
-    float PickupDetectionRange = 300.f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
-    float PickupCollectionThreshold = 50.f;
-
-    UFUNCTION(BlueprintCallable, Category = "Pickup")
-    bool DetectPickup(FVector& vector);
-
-    UFUNCTION(BlueprintCallable, Category = "Movement")
-    FVector GetMovementDirection() const;
-
-    UFUNCTION(BlueprintCallable, Category = "Movement")
-    void ApplyMovement(FVector direction);
-
-    UFUNCTION(BlueprintCallable, Category = "Movement")
-    void ApplyRotation(FVector direction);
-
-    UFUNCTION(BlueprintCallable, Category = "Movement")
-    void UpdateVelocity(float deltaTime);
-
-    //called each frame
-    virtual void Tick(float deltaTime) override;
-
-    bool MoveToTarget(FVector2D target, float speed, float deltaTime);
-    bool DetectObstacle(FVector& normal);
-    void AvoidObstacle(float deltaTime);
+    void DoMovement(float deltaTime, APawn* pawn);
+	void Tick(float dt) override;
 
 private:
+	TArray<FHitResult> GetObstacleInPath();
+	void MoveTowardCollectable();
 
-    UPROPERTY(EditAnywhere)
-    float current_speed = 0.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_max_speed = 600.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_accel = 200.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_radius_detection = 100;
-
-    UPROPERTY(EditAnywhere)
-    float m_wall_detection_distance = 100.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_transition_duration = 3.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_rotation_speed = 15.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_pickup_detection_range = 300.0f;
-
-    UPROPERTY(EditAnywhere)
-    float m_pickup_detection_radius = 300.0f;
-
-    float m_current_transition_duration = 0.0f;
-
-    bool m_wall_detected = false;
-
+protected:
+	FVector m_velocity = FVector::ForwardVector;
+	float m_max_speed = 100;
+	float m_acceleration = 1;
 };
